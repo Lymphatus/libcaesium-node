356e805413f7fba07049581b515338dcaa45aeac
Matteo Paonessa <matteo.paonessa@jetcost.com>
2023-07-27 20:19:45 +0200

    Compress function

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use neon::prelude::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+fn compress(mut cx: FunctionContext) -> JsResult<JsBoolean> {
+    let input = cx.argument::<JsString>(0)?;
+    let output = cx.argument::<JsString>(1)?;
+    let mut compression_parameters = caesium::initialize_parameters();
+    let result: Handle<JsBoolean> = match caesium::compress(input.value(&mut cx), output.value(&mut cx), &compression_parameters) {
+        Ok(_) => cx.boolean(true),
+        Err(_) => cx.boolean(false),
+    };
+
+    Ok(result)
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    cx.export_function("compress", compress)?;
     Ok(())
 }
